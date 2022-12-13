30ec344712b1d854547d5b1e32f588a017193609
Bailey <baileyrosefern@gmail.com>
2022-12-13 10:48:45 -0700

    Added some styling, didn't get the images center yet

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,28 +1,55 @@
 import React from 'react'
 import pokemon from '../models/pokemon'
 
+const headerStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+    textAlign: 'center',
+
+}
+
+const bodyStyle = {
+    backgroundImage: `url("https://i.ytimg.com/vi/ltWyw3JKNsw/maxresdefault.jpg")`,
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+    bakgroundRepeat: 'no-repeat'
+
+}
+
+const listStyle = {
+
+}
+
 function Index({pokemons}) {
   return (
-    <div>
-        <h1>See all the Pokemon! </h1> 
+    <div >
+        <h1 style = {headerStyle}> See all the Pokemon! </h1> 
+        <body style = {bodyStyle}>
         <ul>
             {pokemons.map((pokemon, i) => {
                 return (
                     <li key = {i}>
                         <a href = {`/pokemon/${i}`}>
 
-                            {pokemon.name.toUpperCase()}
+                        {pokemon.name.charAt(0).toUpperCase()}
+                        {pokemon.name.slice(1)} 
                         </a>
-                    
-
-
-                        
+                
                     </li>
                 )
             })}
         </ul>
+
+        </body>
         </div>
+        
   )
 }
 
 export default Index
+
+
+// {pokemons.name.charAt(0).toUpperCase()}
+// {pokemons.name.slice(1)} 
+
+//Making only the first letter be a capital
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import pokemon from '../models/pokemon'
 
+const headingStyle = {
+  color: '#ffffff',
+  backgroundColor: '#000000',
+  textAlign: 'center',
+}
+
+const divStyle = {
+  backgroundImage: `url("https://i.ytimg.com/vi/ltWyw3JKNsw/maxresdefault.jpg")`,
+  backgroundSize: 'cover',
+  overflow: 'hidden'
+}
+
+const headerTwo = {
+  textAlign: 'center',
+
+}
+
+const imageStyle = {
+  alignContent: 'center'
+}
 function Show({pokemons}) {
 
   return (
-    <div> <h1> Gotta Catch 'Em All!</h1>
-           <h2> {pokemons.name.toUpperCase()}</h2>
-           <img src={`${pokemons.img}.jpg`} />
+    <body style= {divStyle}>
+    <div>
+     <h1 style = {headingStyle}> Gotta Catch 'Em All!</h1>
+           <h2 style= {headerTwo}> {pokemons.name.charAt(0).toUpperCase()}
+            {pokemons.name.slice(1)} </h2>
+            <div style = {imageStyle}>
+           <img src={`${pokemons.img}.jpg`}  />
+           </div>
            <a href ='/pokemon'> Back </a>
            
     </div>
+    </body>
   )
 }
 
